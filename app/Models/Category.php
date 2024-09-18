1f62a8206e5b411fade3325a5885431d73f8e613
Damir Khaybulin <damir-khaybulin@mail.ru>
2024-09-18 20:35:54 +0500

    Добавлены описания полей в модели

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,12 +5,43 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Carbon\Carbon;
+
+/**
+ *
+ * @property int $id
+ * @property string $name
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ *
+ */
 
 class Category extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name'];
+    public $table = 'categories';
+
+    protected $fillable = [
+        'name',
+    ];
+
+    protected $casts = [
+        'id' => 'integer',
+        'name' => 'string',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public static function attributeLabels(): array
+    {
+        return [
+            'id' => trans('ID'),
+            'name' => trans('Название'),
+            'created_at' => trans('Создано'),
+            'updated_at' => trans('Обновлено'),
+        ];
+    }
 
     public function tasks(): HasMany
     {
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -1,16 +1,65 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Carbon\Carbon;
+
+/**
+ *
+ * @property int $id
+ * @property string $title
+ * @property null|string $description
+ * @property string $status
+ * @property int $user_id
+ * @property int $category_id
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ *
+ */
 
 class Task extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'description', 'status', 'user_id', 'category_id'];
+    public $table = 'tasks';
+
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'user_id',
+        'category_id'
+    ];
+
+    protected $casts = [
+        'id' => 'integer',
+        'title' => 'string',
+        'description' => 'string',
+        'status' => 'string',
+        'user_id' => 'integer',
+        'category_id' => 'integer',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public static function attributeLabels(): array
+    {
+        return [
+            'id' => trans('ID'),
+            'title' => trans('Название'),
+            'description' => trans('Описание'),
+            'status' => trans('Статус задачи'),
+            'user_id' => trans('Пользователь'),
+            'category_id' => trans('Категория'),
+            'created_at' => trans('Создано'),
+            'updated_at' => trans('Обновлено'),
+        ];
+    }
 
     public function category(): BelongsTo
     {
